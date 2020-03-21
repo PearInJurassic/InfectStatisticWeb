77eeae401425ea56c20e42d2c161931bfe2db262
muxiao167 <60741631+muxiao167@users.noreply.github.com>
2020-03-22 00:00:35 +0800

    Update index.js
    修改API接口，改变读取数据的方式

--- js/index.js
+++ js/index.js
@@ -74,11 +74,12 @@ var dom = document.getElementById("map");
 var myChart = echarts.init(dom);
 var app = {};
 var json;
+var dataList = [];
 
 function fuc() {
     var list
     $.ajax({
-        url:"https://lab.isaaclin.cn/nCoV/api/area",
+        url:"https://lab.ahusmart.com/nCoV/api/area",
         async:false,
         type:"get",
         data:{
@@ -94,14 +95,21 @@ function fuc() {
     })
     return list
 }
-function getNum (province) {
-    var l = fuc()
-    for (var i in l) {
-        if (l[i]["provinceName"] == province) {
-            return l[i]["confirmedCount"]
+
+function f() {
+    dataList = fuc().filter(r => {
+        return r.country == "中国" && r.provinceName !="待明确地区"
+    }).map(r => {
+        return {
+            name:r.provinceShortName,
+            value:r.confirmedCount
         }
-    }
+    })
+
 }
+
+f();
+// console.log(dataList);
 
 option = null;
 option = {
@@ -176,144 +184,7 @@ option = {
                     }
                 }
             },
-            data: [
-                {
-                    value: getNum("湖北省"),
-                    name: "湖北"
-                },
-                {
-                    value: getNum("广东省"),
-                    name: "广东"
-                },
-                {
-                    value: getNum("河南省"),
-                    name: "河南"
-                },
-                {
-                    value: getNum("浙江省"),
-                    name: "浙江"
-                },
-                {
-                    value: getNum("湖南省"),
-                    name: "湖南"
-                },
-                {
-                    value: getNum("安徽省"),
-                    name: "安徽"
-                },
-                {
-                    value: getNum("江西省"),
-                    name: "江西"
-                },
-                {
-                    value: getNum("山东省"),
-                    name: "山东"
-                },
-                {
-                    value: getNum("江苏省"),
-                    name: "江苏"
-                },
-                {
-                    value: getNum("重庆市"),
-                    name: "重庆"
-                },
-                {
-                    value: getNum("四川省"),
-                    name: "四川"
-                },
-                {
-                    value: getNum("黑龙江省"),
-                    name: "黑龙江"
-                },
-                {
-                    value: getNum("北京市"),
-                    name: "北京"
-                },
-                {
-                    value: getNum("上海市"),
-                    name: "上海"
-                },
-                {
-                    value: getNum("河北省"),
-                    name: "河北"
-                },
-                {
-                    value: getNum("福建省"),
-                    name: "福建"
-                },
-                {
-                    value: getNum("广西壮族自治区"),
-                    name: "广西"
-                },
-                {
-                    value: getNum("陕西省"),
-                    name: "陕西"
-                },
-                {
-                    value: getNum("云南省"),
-                    name: "云南"
-                },
-                {
-                    value: getNum("海南省"),
-                    name: "海南"
-                },
-                {
-                    value: getNum("贵州省"),
-                    name: "贵州"
-                },
-                {
-                    value: getNum("天津市"),
-                    name: "天津"
-                },
-                {
-                    value: getNum("山西省"),
-                    name: "山西"
-                },
-                {
-                    value: getNum("辽宁省"),
-                    name: "辽宁"
-                },
-                {
-                    value: getNum("吉林省"),
-                    name: "吉林"
-                },
-                {
-                    value: getNum("甘肃省"),
-                    name: "甘肃"
-                },
-                {
-                    value: getNum("香港"),
-                    name: "香港"
-                },
-                {
-                    value: getNum("新疆维吾尔自治区"),
-                    name: "新疆"
-                },
-                {
-                    value: getNum("内蒙古自治区"),
-                    name: "内蒙古"
-                },
-                {
-                    value: getNum("宁夏回族自治区"),
-                    name: "宁夏"
-                },
-                {
-                    value: getNum("台湾"),
-                    name: "台湾"
-                },
-                {
-                    value: getNum("青海省"),
-                    name: "青海"
-                },
-                {
-                    value: getNum("澳门"),
-                    name: "澳门"
-                },
-                {
-                    value: getNum("西藏自治区"),
-                    name: "西藏"
-                }
-            ]
+            data: dataList
         }
     ]
 };;
